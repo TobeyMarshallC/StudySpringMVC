b2d65972907be80ce2203e2e3c3cce0bfd9bd7fb
TobeyMarshallC <jackluosi@outlook.com>
2017-05-31 15:41:54 +0800

    这是我最新提交的,by JackLuosi HP laptop

---
+++ src/com/springmvc/test/TestStr.java
@@ -0,0 +1,12 @@
+package com.springmvc.test;
+
+/**
+ * Created by Administrator on 2017/5/31.
+ */
+public class TestStr {
+    public static void main(String[] args) {
+        for (int i = 0;i<100;i++){
+            System.out.println(i);
+        }
+    }
+}
