1841e5729ccb09bc2efffd34276e4c2ce52d2cee
TobeyMarshallC <jackluosi@outlook.com>
2017-07-31 21:19:45 +0800

    update 2017/05/31

--- src/com/springmvc/core/handlers/MyHandlers.java
+++ src/com/springmvc/core/handlers/MyHandlers.java
@@ -1,14 +1,28 @@
 package com.springmvc.core.handlers;
 
+import com.springmvc.core.service.MyServiceImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.annotation.Resource;
+import java.util.Map;
 
 @Controller
+@RequestMapping("my")
 public class MyHandlers {
 
+	@Resource(name="myService")
+	private MyServiceImpl myService;
+
+
 	@RequestMapping("/helloworld")
-	public String hello(){
+	public ModelAndView hello(){
+		Map resultMap = myService.getNowData(null);
 		System.out.println("hahaha");
-		return "success";
+
+		ModelAndView mav = new ModelAndView("success");
+		mav.addObject("nowdate",resultMap.get("Now_date"));
+		return  mav;
 	}
 }
