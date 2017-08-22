1bacf8290bc7282922d32a2e04cd8bf4760a8581
TobeyMarshallC <jackluosi@outlook.com>
2017-08-22 17:29:29 +0800

    From HP Computer

--- src/com/springmvc/test/redis/RedisExample.java
+++ src/com/springmvc/test/redis/RedisExample.java
@@ -2,6 +2,8 @@ package com.springmvc.test.redis;
 
 import redis.clients.jedis.Jedis;
 
+import java.util.List;
+
 /**
  * Created by Administrator on 2017/8/22.
  */
@@ -16,6 +18,37 @@ public class RedisExample {
         //查看服务是否运行
         System.out.println("服务是否运行：" + jedis.ping());
         System.out.println(jedis.get("username"));
+
+        //存储List缓存数据
+        jedis.lpush("booklist","你是对的");
+        jedis.lpush("booklist","哈哈");
+        jedis.lpush("booklist","嘿嘿");
+
+        //获取list缓存数据
+        List<String> listCache = jedis.lrange("booklist",0,3);
+        for (int i = 0;i<listCache.size();i++){
+            System.out.println("缓存输出："+listCache.get(i));
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     }
 }
