65c5ebd28a24e5163560a996eda66ce22a9e3ced
TobeyMarshallC <jackluosi@outlook.com>
2017-09-03 22:03:46 +0800

    update 2017/05/31

--- src/com/springmvc/test/redis/TestRedis.java
+++ src/com/springmvc/test/redis/TestRedis.java
@@ -10,6 +10,8 @@ public class TestRedis {
         Jedis jedis = null;
         try{
             jedis = new Jedis("127.0.0.1",6379);
+            System.out.println("连接成功");
+            System.out.println("服务器是否运行："+jedis.ping());
         }catch (Exception ex){
             ex.printStackTrace();
         }finally {
